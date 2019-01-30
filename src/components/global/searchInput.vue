7d41bf4864d60973e3859700fddfef0055bf8a58
no-Coke-no-Code <1456751226@qq.com>
2019-01-30 11:48:19 +0800

    修改搜索框组件的样式

--- src/components/global/searchInput.vue
+++ src/components/global/searchInput.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        <el-input v-model="searchText" placeholder="请输入搜索内容"><el-button slot="append" type="primary" @click="searching"><i class="el-icon-search"></i>搜索</el-button></el-input>
+    <div class="searchInputWrapper">
+        <el-input v-model="searchText" placeholder="请输入搜索内容">
+            <template slot="append">
+                <el-button type="primary" class="searchBtn" @click="searching"><i class="el-icon-search"></i>搜索</el-button>
+            </template>
+        </el-input>
+        <!-- <el-button type="primary" class="searchBtn" @click="searching"><i class="el-icon-search"></i>搜索</el-button> -->
     </div>
 </template>
 
@@ -20,6 +25,41 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
 
+<style>
+    .searchInputWrapper .el-input__inner
+    {
+        border-right: transparent !important;
+    }
+</style>
+
+<style lang="scss" scoped>
+    .searchInputWrapper
+    {
+        margin: 50px 200px 30px 200px;
+        position: relative;
+        text-align: center;
+        .el-input
+        {
+            width: 500px;
+            padding: 5px;
+            margin: 0px auto;
+        }
+        .el-button
+        {
+            background: #409EFF;
+            color: white;
+            font-weight: bold;
+            border-top-left-radius: 0 !important;
+            border-bottom-left-radius: 0 !important;
+            [class^=el-icon-]
+            {
+                font-weight: bold;
+            }
+            &:hover
+            {
+                background-color:rgb(141, 198, 255)  !important;
+            }
+        }
+    }
 </style>
